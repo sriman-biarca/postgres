813d983d006c7a75dbaedca059f15799ee5f26b2
manikantanr <manikantanr@biarca.com>
2018-05-09 01:58:06 +0530

    added script files

--- script1.sh
+++ script1.sh
@@ -1,3 +1,4 @@
 set -x
 hostname
 hostname -I
+for i in {0..15}; do echo  "$i"_script1; sleep 1; done; echo
--- script2.sh
+++ script2.sh
@@ -1,3 +1,4 @@
 set -x
 hostname
 hostname -I
+for i in {0..15}; do echo  "$i"_script2; sleep 1; done; echo
