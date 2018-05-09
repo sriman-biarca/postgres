4bcda4350c0b6423cfb1071ab147c4549ea6b99f
sriman-biarca <srimanc@biarca.com>
2018-05-09 15:59:01 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,7 +1,6 @@
-hostname
-hostname -I
-ls -l /workspace
+cat /workspace/ip.txt
+ip=`cat /workspace/ip.txt`
+echo $ip
 for i in {0..15}; do echo  "$i"_script2; sleep 1; done; echo
 
-
-ls -l /workspace
+psql -U postgres -W -h $ip -c "create user test with password 'test';"
