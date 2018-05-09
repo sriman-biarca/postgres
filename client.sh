81b4585187c4896ce47d0a23b08067b8b07d5414
sriman-biarca <srimanc@biarca.com>
2018-05-09 16:02:21 +0530

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,8 +1,6 @@
-hostname
-ip=`hostname -I`
-
-ls -l /workspace
-echo $ip > /workspace/ip.txt
 cat /workspace/ip.txt
+ip=`cat /workspace/ip.txt`
+echo $ip
+for i in {0..15}; do echo  "$i"_script2; sleep 1; done; echo
 
-for i in {0..15}; do echo  "$i"_script1; sleep 1; done; echo
+psql -U postgres -W test1234 -h $ip -c "create user test with password 'test';"
