c4ef8eb61ea1c69b7748f8e061c11979e504f164
sriman-biarca <srimanc@biarca.com>
2018-05-10 11:37:47 +0530

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -5,7 +5,8 @@ echo "IP=`hostname -I`" > /workspace/ip.txt
 cat /workspace/ip.txt
 source /workspace/ip.txt
 echo $IP
-
+service postgresql status
+echo "service test"
 sudo service postgresql status
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 sudo service postgresql status
