4e09a7bac1a847ee0729c60e5272988a84b5d21f
sriman-biarca <srimanc@biarca.com>
2018-05-10 12:05:24 +0530

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,4 +1,5 @@
 source /workspace/ip.txt
 echo $IP
 
-PG_PASSWORD=postgres psql -U postgres -h $IP -c "create user test with password 'test';"
+#PGPASSWORD=postgres psql -U postgres -h $IP -c "create user test with password 'test';"
+PGPASSWORD=postgres psql -U postgres -h 192.168.10.2 -c "create user test with password 'test';
