e852dda64c7c5f5a67a78704cc6b3969d77e83aa
sriman-biarca <srimanc@biarca.com>
2018-05-10 13:19:54 +0530

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+sudo chown -R postgres:postgres /var/lib/postgresql/10/
+sudo chmod -R u=rwX,go= /var/lib/postgresql/10/
+
+sudo service postgresql restart
+
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 
 # Restart so that all new config is loaded:
-sudo service postgresql restart
+#sudo service postgresql restart
 
 ls -l /workspace
 echo "IP=`hostname -I`" > /workspace/ip.txt
