5db039530e621dfcf6075177df805259868b0611
sriman-biarca <srimanc@biarca.com>
2018-05-10 13:44:06 +0530

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -3,9 +3,6 @@ PG_VERSION=10
 sudo chown -R postgres:postgres /var/lib/postgresql/10/
 sudo chmod -R u=rwX,go= /var/lib/postgresql/10/
 
-ls -la /var/lib/postgresql/10/main/
-
-sudo cat /var/lib/postgresql/10/main/pg_hba.conf
 
 echo "host    all             all             0.0.0.0/0                    md5" |  sudo tee -a "/etc/postgresql/$PG_VERSION/main/pg_hba.conf"
 
@@ -15,6 +12,7 @@ sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 
 # Restart so that all new config is loaded:
 #sudo service postgresql restart
+netstat -ant | findstr 5432
 
 ls -l /workspace
 echo "IP=`hostname -I`" > /workspace/ip.txt
