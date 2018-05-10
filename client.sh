6af89c979bd8e5527b57f331ae02eebd3bfafff1
sriman-biarca <srimanc@biarca.com>
2018-05-10 13:54:42 +0530

    Update client.sh

--- client.sh
+++ client.sh
@@ -9,5 +9,8 @@ sudo chmod -R u=rwX,go= /var/lib/postgresql/10/
 # Restart so that all new config is loaded:
 sudo service postgresql restart
 
+echo "postgres" | psql -U postgres -h 192.168.10.2 -c "create user test with password 'test';"
+
+echo "test"
 #PGPASSWORD=postgres psql -U postgres -h $IP -c "create user test with password 'test';"
 PGPASSWORD=postgres psql -U postgres -h 192.168.10.2 -c "create user test with password 'test';"
