e78f596735a7e47e21acd904ab8053f5f0338886
sriman-biarca <srimanc@biarca.com>
2018-05-10 14:44:17 +0530

    Update client.sh

--- client.sh
+++ client.sh
@@ -18,5 +18,7 @@ sudo service postgresql restart
 echo "test1"
 #psql -U postgres -w -h 192.168.10.2 -c "create user test with password 'test';"
 echo "test2"
+
+sleep(120)
 #PGPASSWORD=postgres psql -U postgres -h $IP -c "create user test with password 'test';"
 PGPASSWORD=postgres psql -U postgres -h 192.168.10.2 -c "create user test with password 'test';"
