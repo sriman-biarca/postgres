9b5f13d1d45e5f000d1a3d737c748a9e75131018
sriman-biarca <srimanc@biarca.com>
2018-05-10 13:27:27 +0530

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-
+PG_VERSION=10
 sudo chown -R postgres:postgres /var/lib/postgresql/10/
 sudo chmod -R u=rwX,go= /var/lib/postgresql/10/
 
+
+sudo cat /var/lib/pgsql/10/data/postgresql.conf
+
+
+sudo cat  /etc/postgresql/$PG_VERSION/main/pg_hba.conf
+
+sudo cat  /etc/postgresql/$PG_VERSION/main/postgresql.conf
 sudo service postgresql restart
 
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
